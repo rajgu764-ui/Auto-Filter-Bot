6a63ff1cf20c266e1a2d8ac268bdb75800d24d40
rajgu764-ui <rajgu764@gmail.com>
2026-08-13 07:24:32 +0600

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 AUTH_CHANNELS = [int(ch) for ch in environ.get("AUTH_CHANNELS", "-1003748603064").split() if ch and id_pattern.match(ch)]
 AUTH_REQ_CHANNELS = [int(ch) for ch in environ.get("AUTH_REQ_CHANNELS", "-1004295925966").split() if ch and id_pattern.match(ch)]
 REQST_CHANNEL = int(ch) if (ch := environ.get("REQST_CHANNEL", "-1004418130323")) and id_pattern.search(ch) else None
-SUPPORT_CHAT_ID = int(ch) if (ch := environ.get("SUPPORT_CHAT_ID", "-1003946294006")) and id_pattern.search(ch) else None
+SUPPORT_CHAT_ID = int(ch) if (ch := environ.get("SUPPORT_CHAT_ID", "4293982912")) and id_pattern.search(ch) else None
 
 OWNER = int(os.environ.get("OWNER", "8747490676"))
 CHANNEL_LINK = environ.get('CHANNEL_LINK', 'https://telegram.me/CineqaroUpdateChannel')
